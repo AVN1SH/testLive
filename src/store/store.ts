34acf8b08f0e2b56fb0c08d4f589d1c6c7725d02
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-06-16 15:54:21 +0530

    test

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice, { AuthState } from "@/features/authSlice";
 import stdSlice, { StdState } from "@/features/stdSlice";
 
 export interface RootState {
-  authSlice : AuthState;
   stdSlice : StdState;
 }
 
 const store = configureStore({
   // reducer: authSlice
   reducer : {
-    authSlice,
     stdSlice
   }
 })
